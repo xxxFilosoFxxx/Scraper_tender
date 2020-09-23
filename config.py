254a092715c2f695a88b51a33c59cf69c8ae4254
xxxFilosoFxxx <roman.lashnev@mail.ru>
2020-09-23 05:03:40 +0300

    wtf, href=about:blank...

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 
-SEARCH_KEY = os.environ.get('SEARCH_KEY', 'тендер')
+SEARCH_KEY = os.environ.get('SEARCH_KEY', 'молоко')
--- parse_sber_ats.py
+++ parse_sber_ats.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
@@ -31,6 +32,42 @@ def tender1(param):
     print(search_form)
 
 
+# Обрабатывает ссылки нужных тендеров
+def href_trade_all(driver, i=0):
+    href_list = []
+    while i < 100:
+        href_all_elem = WebDriverWait(driver, 0.5).until(
+                EC.presence_of_all_elements_located((
+                    By.XPATH, "//form[@id='aspnetForm']/div[@class='master_open_content']/"
+                              "div/div/div[@id='resultTable']/div/"
+                              "div[@class='purch-reestr-tbl-div']/"
+                              "table[@class='es-reestr-tbl its']/tbody/"
+                              "tr[@class='dotted-botom last']/td/div[@class='element-in-one-row']")))
+        try:
+            WebDriverWait(href_all_elem[i], 0.1).until(EC.presence_of_all_elements_located((
+                    By.XPATH, "div[@style='display: inline;']/input[@value='Подать заявку']")))
+
+            button = WebDriverWait(href_all_elem[i], 0.2).until(EC.element_to_be_clickable((
+                By.XPATH, "input[@type='button' and @value='/  Просмотр']")))
+
+            # button = href_all_elem[i].find_element_by_xpath("input[@type='button' and @value='/  Просмотр']")
+            driver.execute_script("arguments[0].click();", button)
+            driver.switch_to_window(driver.window_handles[-1])
+            url = str(driver.current_url)
+            href_list.append(url)
+            driver.close()
+            driver.switch_to_window(driver.window_handles[0])
+        except StaleElementReferenceException:
+            pass
+        except TimeoutException:
+            pass
+        i += 1
+    # for j in href_list:
+    #     print(j)
+    # TODO: Записывает ссылки, и потом они преобразовываются в None
+    return href_list
+
+
 # Обрабатывает ошибки DOM дерева
 def trade_all(driver, index=0):
     while True:
@@ -40,25 +77,7 @@ def trade_all(driver, index=0):
             "div[@class='purch-reestr-tbl-div']")  # может взять с [index]
         if index >= len(all_elem):
             break
-        # TODO: Переключение страниц
-        # href_all_elem = WebDriverWait(all_elem, 0.5).until(
-        #     EC.element_to_be_clickable((
-        #         By.XPATH, "table[@class='es-reestr-tbl its']/tbody/"
-        #                   "tr[@class='dotted-botom last']/td/div/"
-        #                   "div[@style='display: inline;']/"
-        #                   "input[@value='/  Просмотр']"
-        #         ))).click()
-
-        # href_all_elem = driver.find_elements_by_xpath(
-        #     "table[@class='es-reestr-tbl its']/tbody/"
-        #     "tr[@class='dotted-botom last']/td/div/"
-        #     "div[@style='display: inline;']/"
-        #     "input[@value='/  Просмотр']"
-        # )
-
-        # if index >= len(href_all_elem):
-        #     break
-        yield all_elem[index]  #, href_all_elem[index]
+        yield all_elem[index]
         index += 1
 
 
@@ -70,18 +89,22 @@ def word_processing(data):
     for reg in data:
         may_be = string.findall(reg)
         negative_facts = ["Рассмотрение", "Завершено", "Завершен(-а)", "Отменено", "Отменен(-а)",
-                          "Не", "В", "Подведение", "Опубликован(-а)"]
+                          "Не", "В", "Подведение", "Опубликован(-а)", "Tорговый", "Работа"]
+        # true_facts = ["Подача заявок", "Прием заявок"]
         if all(l not in may_be[3].replace('\n', '').split()[0] for l in negative_facts):
             checked.append(string.findall(reg))
     return checked
 
 
-# TODO: сделать ассинхронные запросы
+# TODO: сделать ассинхронные запросы, если возможно
 def selenium_parse1(param):
     options = Options()
     options.headless = True
     driver = webdriver.Firefox(options=options)  # /usr/local/bin
     # driver = webdriver.Firefox()
+    # driver.maximize_window()
+    # driver.implicitly_wait(20)
+
     driver.get("http://sberbank-ast.ru")
     driver.implicitly_wait(1)  # seconds
     map_table = []
@@ -100,24 +123,28 @@ def selenium_parse1(param):
         all_options = element_select.find_elements_by_tag_name("option")
         all_options[2].click()  # Выставление 100 значений на странице
 
-        for table in trade_all(driver):
-            map_table.append(table.text)
+        for table in href_trade_all(driver):
+            map_table.append(table)
 
-        start_time = time.time()
-        reg_table = word_processing(map_table)
-        for i in reg_table:
-            if i[6] != '\n':
-                clear_table.append([i[4], i[0], i[2], i[6], i[-2]])
-        print("%f seconds" % (time.time() - start_time))
+        # start_time = time.time()
+        # reg_table = word_processing(map_table)
+        # for i in reg_table:
+        #     if i[6] != '\n':
+        #         clear_table.append([i[4], i[0], i[2], i[6], i[-2]])
+        # print("%f seconds" % (time.time() - start_time))
 
         # page = driver.page_source
         # soup = BeautifulSoup(page, 'html')
         # print(soup)
     finally:
-        for i in range(len(clear_table)):
-            print(clear_table[i])
+        # for i in range(len(clear_table)):
+        #     print(clear_table[i])
+        #     print('<----------------------------->')
+        # print(len(clear_table))
+        for i in range(len(map_table)):
+            print(map_table[i])
             print('<----------------------------->')
-        print(len(clear_table))
+        print(len(map_table))
         driver.close()
 
 
